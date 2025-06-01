9e2cbaede5c14bdce05bf3b1963f6d681d7d7a4b
Adnan FAIZE <adnanfaize@gmail.com>
2025-06-01 15:31:28 +0200

    Simplify engine test flag initialization in Launcher

--- game/src/main/java/games/temporalstudio/timecapsule/Launcher.java
+++ game/src/main/java/games/temporalstudio/timecapsule/Launcher.java
@@ -5,12 +5,9 @@ import games.temporalstudio.timecapsule.test.TestGame;
 public class Launcher{
 
 	public static void main(String[] args){
-		boolean isEngineTest = false;
+		boolean isEngineTest = args.length > 0 && args[0].equalsIgnoreCase("engine");
 
-		if(args.length > 0 && args[0].equalsIgnoreCase("engine"))
-			isEngineTest = true;
-
-		if(!isEngineTest)
+        if(!isEngineTest)
 			new TimeCapsule().run(new String[]{"Time Capsule"});
 		else
 			new TestGame().run(new String[0]);
